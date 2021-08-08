8b8e651d1666aaa4becb6e8500b787b45ed3e918
yuriy.lenchenkov <yuriy.lenchenkov@gmail.com>
2021-08-08 23:29:05 +0300

    hw5 editted

--- hw5.py
+++ hw5.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 # Minimum missed number function
 def min_num (o):
     sort = sorted(o)
@@ -8,21 +7,31 @@ def min_num (o):
         if (sort[k] in sort) and ((sort[k] + 1) not in sort):
             break
         else:
-            print('missed number is ', min_num(inp_num))
+            print('missed number is ', min_num(int(inp)))
             k += 1
     return sort[k] + 1
 # input numbers from user
 inp = input('Please enter space-delimited non-negative integer numbers: ').split(' ')
-
 # Exceptions catching
-# catching negative number
-try:
-    inp_num = [*map(int, inp)]
-    for i in inp_num:
-        if i < 0:
-            print('negative number exists, please enter again')
-            exit()
-
-    print('missed number is ', min_num(inp_num))
-except:
-    print('Please correct your mistakes')
+# catching negative numbers
+for i in inp:
+    if int(i) < 0:
+        print('negative number exists, please enter again')
+        exit()
+# catching duplicates
+d = {}
+for i in inp:
+    d.update({i: inp.count(i)})
+if len(d) < len(inp):
+    print('duplicate values, please enter again')
+    exit()
+# catching no holes
+inp_s = sorted(inp)
+delta = (int(inp_s[1])) - (int(inp_s[0]))
+t_min = int(min(inp_s))
+t_max = int(max(inp_s))
+range_list = list(range(t_min, t_max+1, delta))
+if (''.join(str(inp).replace("'",''))) == (''.join(str(range_list))):
+    print('no holes')
+    exit()
+print('missed number is ', min_num(inp))
