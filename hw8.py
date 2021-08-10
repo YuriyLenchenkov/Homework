dd114a6e76f7fb0ebad8913b5eb04fb1d5f76860
yuriy.lenchenkov <yuriy.lenchenkov@gmail.com>
2021-08-10 23:09:27 +0300

    hw8 added

---
+++ hw8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def numcheck(o):
+    if o.lower() == 'cancel':
+        return 'cancel'
+    try:
+        int(o)
+    except ValueError:
+        return 'Не удалось преобразовать введенный текст в число.'
+    r = int(inp)
+    if (r % 2) == 0:
+        return r//2
+    elif (r % 2) > 0:
+        return r*3+1
+
+
+inp = input('please enter something ')
+print(numcheck(inp))
