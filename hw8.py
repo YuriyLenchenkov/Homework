ceec613f0440e092977db0092be922956fe2b7f4
yuriy.lenchenkov <yuriy.lenchenkov@gmail.com>
2021-08-11 12:09:23 +0300

    hw8 edited

--- hw8.py
+++ hw8.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
-def numcheck(o):
-    if o.lower() == 'cancel':
-        print('cancel')
+def numcheck(r):
+    r = int(r)
+    if (r % 2) == 0:
+        k = 0
+        while k < (r//2):
+            numcheck(r/2)
+            k += 1
+        return k
+    elif (r / 2) > 0:
+        k = 0
+        while k <= (r*3):
+            numcheck(r/3)
+            k += 1
+        return k
+
+
+o = input('please enter smth ')
+if o.lower() == 'cancel':
+    print('cancel')
+    exit()
+else:
+    try:
+        int(o)
+    except ValueError:
+        print('Не удалось преобразовать введенный текст в число.')
         exit()
-    else:
-        try:
-            int(o)
-        except ValueError:
-            print('Не удалось преобразовать введенный текст в число.')
-            exit()
-    if (int(o) % 2) == 0:
-        print(int(o)//2)
-    elif (int(o) % 2) > 0:
-        print(int(o)*3+1)
-
-
-numcheck(input('please enter something '))
+print(numcheck(o))
