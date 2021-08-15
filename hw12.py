a30ed9a93318a45f26b89750988267f29cc5480b
yuriy.lenchenkov <yuriy.lenchenkov@gmail.com>
2021-08-15 23:38:52 +0300

    hw 12 edited

--- hw12.py
+++ hw12.py
@@ -6,6 +6,9 @@ def fib(inp):
         print('you should start from 1 element')
         exit()
     else:
+# very slow :(
+#        fibon = lambda inp: inp if inp <= 1 else fibon(inp - 1) + fibon(inp - 2)
+#        print(list(map(fibon, range(1, inp+1, 1))))
         counter = 2
         while counter < inp:
             o.append(o[counter-1] + o[counter-2])
@@ -16,3 +19,4 @@ def fib(inp):
 o = [1, 1]
 inp = int(input('please enter number '))
 print(f'{inp} element of fibonacci is ', fib(inp))
+#print(f'{inp} element of fibonacci is ', fib(inp)[inp-1])
