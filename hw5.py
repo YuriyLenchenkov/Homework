c6b97909ec83f586797584b7df83d67b3aea76a9
yuriy.lenchenkov <yuriy.lenchenkov@gmail.com>
2021-09-18 16:52:00 +0300

    hw5 edited

--- hw5.py
+++ hw5.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 # Minimum missed number function
-def min_num (o):
+def min_num(o):
     sort = sorted(o)
+#    print(sorted(o))
     k = 0
     for i in sort:
-        if (sort[k] in sort) and (((sort[k]) + 1) not in sort):
-            break
-        else:
-            print('missed number is ', min_num(inp))
+        if (sort[k] in sort) and ((sort[k] + 1) in sort):
             k += 1
-    return sort[k] + 1
+#    print(sort[k], sort[k] + 1, k)
+#
+        else:
+#            print('missed number is ', min_num(inp))
+#            k += 1
+            return sort[k] + 1
 # input numbers from user
 inp = (input('Please enter space-delimited non-negative integer numbers: ')).split(' ')
-inp_int=list(map(int, inp))
-print(inp_int)
+inp_int = list(map(int, inp))
+# print(inp_int)
 # Exceptions catching
 # catching negative numbers
 for i in inp:
@@ -21,18 +24,19 @@ for i in inp:
         print('negative number exists, please enter again')
         exit()
 # catching duplicates
-d = {}
-for i in inp:
-    d.update({i: inp.count(i)})
-if len(d) < len(inp):
-    print('duplicate values, please enter again')
-    exit()
+#d = {}
+#for i in inp:
+#    d.update({i: inp.count(i)})
+#if len(d) < len(inp):
+#    print('duplicate values, please enter again')
+#    exit()
 # catching no holes
-t_min = min(inp_int)
-t_max = max(inp_int)+1
-range_list = list(range(t_min, t_max))
-if (sum(inp_int) == sum(range_list)):
-    print('no holes, enter again ')
-    exit()
+#t_min = min(inp_int)
+#t_max = max(inp_int)+1
+#range_list = list(range(t_min, t_max))
+#if (sum(inp_int) == sum(range_list)):
+#    print('no holes, enter again ')
+#    exit()
 #printing missed number after checks
 print('missed number is ', min_num(inp_int))
+
