b31da659526d28cd36e95c06a6a008318745fb9c
yuriy.lenchenkov <yuriy.lenchenkov@gmail.com>
2021-09-27 22:01:57 +0300

    hw7 with  empty groups updated

--- hw7.py
+++ hw7.py
@@ -13,6 +13,15 @@ count_shell = Counter(shell)
 print(str(count_shell).replace('Counter({', '').replace(':', ' -').replace("'", "").replace(',', ';').replace('})', ''))
 print("\n")
 
+gr_empty = {}
+with open('/etc/group') as csv_file:
+    csv_group = csv.reader(csv_file, delimiter=':')
+    for row in csv_group:
+        if len(row[3]) == 0:
+            gr_empty.update({row[0]:row[2]})
+    gr_empty_str = str(gr_empty).replace("': '", ":").replace("'","").replace("{","").replace("}","")
+    print(gr_empty_str, end="")
+
 for i in user:
 
     command = "id -G " + i
